45e06295b0a72455e2fd557902bfe5c5794e1d9f
Austin Powell <apst12012@gmail.com>
2019-12-16 11:58:09 -0500

    Auth0 implementation, custom hooks, login/redirects and placeholder components Co-authored-by: Lorenzo simpson <lorenzosimp@gmail.com> Co-authored-by: Alec blakeley <Alecblkly@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
+import NavBar from './components/NavBar';
+import Profile from './components/Profile';
 
 
 function App() {
@@ -10,7 +14,16 @@ function App() {
   
   return (
     <div className="App">
-      <header>Welcome to our App!</header>
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -11,15 +11,34 @@ import thunk from "redux-thunk";
 
 import reducer from "./reducers/reducer"
 
+import { Auth0Provider } from "./auth0-hooks/react-auth0-spa";
+import config from "./utils/auth_config.json";
+import history from "./utils/history";
+
 const store = createStore(reducer, applyMiddleware(thunk))
 
 Sentry.init({dsn: "https://26bacf2042764db6a7a1af49e8dde7e1@sentry.io/1855868"});
 
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+    history.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  };
+
 ReactDOM.render(
+    <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}>
     <Provider store={store}>
         <App />
     </Provider>
-
+    </Auth0Provider>
 , 
 document.getElementById('root'));
 
