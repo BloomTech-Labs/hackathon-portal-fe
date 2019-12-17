631b685988cf55f7da44616781309d8d8d092772
Hector-bit <hectormartinez.2978@gmail.com>
2019-12-17 10:31:09 -0800

    added more styling INPROGRESS

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div className='eventsList'>
-            <span class='createEventButton'>Create Event</span>
+        <div className='dashboard'>
+            <h3>Dashboard</h3>
+            <div className='eventsList'>
+                <div class='createEventButton' onClick={console.log('create event button clicked')}>Create Event</div>
+            </div>
         </div>
     )
 }
