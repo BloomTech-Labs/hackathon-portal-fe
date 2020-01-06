aabe39c0d809ec9f69448f90d5de0a2947e02a12
Alec Blakeley <alecblkly@gmail.com>
2020-01-06 15:38:59 -0500

    Moved reducer folder from outside of component. Co-Authored-By: Lorenzo Simpson <lorenzosimp@gmail.com> Co-Authored-By: Jeremy Rogel <jeremyrogel@ymail.com> Co-Authored-By: Alec Blakeley <alecblkly@gmail.com> Co-Authored-By: Austin Powel <Apst12012@gmail.com> Co-Authored-By: Hector Martinez <hectormartinez.2978@gmail.com> Co-Authored-By: Jamea Kidrick <meakidrick@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,44 +4,41 @@ import './index.css';
 // import * as Sentry from '@sentry/browser';
 import App from './App';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-import {createStore, applyMiddleware} from "redux";
-import {Provider} from "react-redux";
-import thunk from "redux-thunk";
+import reducer from './reducers/reducer';
 
-import reducer from "./reducers/reducer"
+import { Auth0Provider } from './auth0-hooks/react-auth0-spa';
+import config from './utils/auth_config.json';
+import history from './utils/history';
 
-import { Auth0Provider } from "./auth0-hooks/react-auth0-spa";
-import config from "./utils/auth_config.json";
-import history from "./utils/history";
-
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, applyMiddleware(thunk));
 
 // Sentry.init({dsn: "https://26bacf2042764db6a7a1af49e8dde7e1@sentry.io/1855868"});
 
 // A function that routes the user to the right place
 // after login
 const onRedirectCallback = appState => {
-    history.push(
+   history.push(
       appState && appState.targetUrl
-        ? appState.targetUrl
-        : window.location.pathname
-    );
-  };
+         ? appState.targetUrl
+         : window.location.pathname
+   );
+};
 
 ReactDOM.render(
-    <Auth0Provider
-    domain={config.domain}
-    client_id={config.clientId}
-    redirect_uri={window.location.origin}
-    audience={config.audience}
-    onRedirectCallback={onRedirectCallback}>
-    <Provider store={store}>
-        <App />
-    </Provider>
-    </Auth0Provider>
-, 
-document.getElementById('root'));
-
-
-
+   <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      redirect_uri={window.location.origin}
+      audience={config.audience}
+      onRedirectCallback={onRedirectCallback}
+   >
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </Auth0Provider>,
+   document.getElementById('root')
+);
