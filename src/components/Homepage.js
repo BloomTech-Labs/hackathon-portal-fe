e9e91856f7122f7b4109227e1c058140b9ffd98b
Hector-bit <hectormartinez.2978@gmail.com>
2020-01-08 15:24:11 -0800

    making cards and making calls to api

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,70 @@
-import React from 'react';
-import{ Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getHackathons } from '../actions/actions';
+import { Link } from 'react-router-dom';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles(theme => ({
+   card: {
+      
+      maxWidth: '30%',
+      margin: '10%',
+
+      background: '#1c1c1f',
+      border: '1px solid #D0DDFF',
+      boxSizing: 'border-box',
+      borderRadius: '13.5px',
+      color: '#D0DDFF',
+
+
+   },
+   cardparent: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    
+  },
+
+   media: {
+      height: 0,
+      paddingTop: '56.25%' // 16:9
+   },
+   expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+         duration: theme.transitions.duration.shortest
+      })
+   },
+   expandOpen: {
+      transform: 'rotate(180deg)'
+   },
+   avatar: {
+      backgroundColor: red[500]
+   }
+}));
 
 const Homepage = () => {
+        const classes = useStyles();
+        const isFetching = useSelector(state => state.isFetching);
+        const dispatch = useDispatch();
+        const hackathons = useSelector(state => state.hackathons);
+     
+        console.log(hackathons);
+     
+        useEffect(() => {
+           dispatch(getHackathons());
+        }, []);
+     
+        if (isFetching) {
+           return <h2>Loading Events...</h2>;
+        }
+
     return(
         <div className='Homepage'>
             <section className='carosel'>
@@ -9,6 +72,13 @@ const Homepage = () => {
             </section>
             <section>
                 <h2>Open Hackathons</h2>
+                <div className='openHackathonsHolder'>
+                    <div className='openHackathonCard'>
+                        <img src='#' alt='Hackthon picture'></img>
+                        <h4>Big Hackthon</h4>
+                        <p>Location: Based in Vancouver</p>
+                    </div>
+                </div>
             </section>
         </div>
     )
