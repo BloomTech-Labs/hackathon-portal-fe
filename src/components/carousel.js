3dee3028fbfc09605e169f9de63f02bf6a8657bf
Austin powell <apst12012@gmail.com>
2020-01-10 20:42:03 -0500

    zombie code removed

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -26,42 +26,9 @@ const tutorialSteps = [
     label: 'Description: Bali, Indonesia',
     imgPath:
       'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-  },
-  // {
-  //   label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-  //   imgPath:
-  //     'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
-  // },
-  // {
-  //   label: 'Goč, Serbia',
-  //   imgPath:
-  //     'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  // },
+  }
 ];
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     justifyContent: 'center',
-//     maxWidth: '70%',
-//     flexGrow: 1,
-//   },
-//   header: {
-//     display: 'flex',
-//     alignItems: 'center',
-//     height: 50,
-//     paddingLeft: theme.spacing(4),
-//     // backgroundColor: theme.palette.background.default,
-//   },
-//   img: {
-//     position: 'absolute',
-//     left: '0%',
-//     right: '0%',
-//     top: '0%',
-//     bottom: '0%',
-//   },
-// }));
 
 function Carousel() {
  
