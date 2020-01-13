2730ff72c2fde06dcdc148dc98eba58015d8b017
Austin Powell <apst12012@gmail.com>
2020-01-13 17:49:06 -0500

    Fixed userprofile hackathon styles, edit hackathon form style, delete hackathon modal

--- src/components/Organizers/DeleteHackathon.js
+++ src/components/Organizers/DeleteHackathon.js
@@ -1,66 +1,61 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
+import React, {useState} from 'react';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  // Button,
+  TextField
+} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import { useDispatch } from 'react-redux';
 
 // ACTIONS
 import { deleteHackathon } from '../../actions/actions'
 
-const useStyles = makeStyles(theme => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
 
 export default function DeleteHackathonModal(props) {
   const dispatch = useDispatch();
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+  const [deleteOpen, setDeleteOpen] = useState(false);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const handleDeleteClick = () => {
+    setDeleteOpen(true);
+ };
+
+ const handleDeleteClose = () => {
+    setDeleteOpen(false);
+ };
 
   return (
     <div>
-      <button type="button" onClick={handleOpen}>
+      <button type="button" onClick={handleDeleteClick}>
         DELETE
       </button>
-      <Modal
-        aria-labelledby="delete-hackathon"
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <h2 id="transition-modal-title">Deleting Hackathon</h2>
-            <p id="transition-modal-description">Are you sure you want to delete this event? This can not be undone.</p>
-            <button onClick={()=>dispatch(deleteHackathon(props.id, props.org_id, props.history))}>YES, DELETE</button>
-            <button onClick={()=>handleClose()}>NO, CANCEL</button>
-            
-          </div>
-        </Fade>
-      </Modal>
+
+      <Dialog
+            open={deleteOpen}
+            onClose={handleDeleteClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+         >
+            <DialogTitle id="alert-dialog-title">
+               {'Are you sure you want to delete this event? This cannot be undone.'}
+            </DialogTitle>
+            <DialogActions>
+               <Button onClick={handleDeleteClose} color="primary">
+               NO, CANCEL
+               </Button>
+               <Button
+                  onClick={()=>dispatch(deleteHackathon(props.id, props.org_id, props.history))
+                  }
+                  color="primary"
+                  autoFocus
+               >
+                 YES, DELETE
+               </Button>
+            </DialogActions>
+         </Dialog>
     </div>
   );
 }
