71bafaf395c93e64a96a1facde7f6e97963c7094
Austin powell <apst12012@gmail.com>
2020-01-13 11:53:11 -0500

    zombie code deletion

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -40,8 +40,6 @@ const Homepage = () => {
     return(
         <div className='Homepage'>
                 <Carousel className='carousel'></Carousel>
-                {/* <div className='leftArrow' onClick={handleBack, console.log('yeah this was clicked')} disabled={activeStep === 2}>></div>
-                <div className='rightArrow' onClick={handleNext} disabled={activeStep === 0}>></div> */}
 
                 <div id='openhackathon'>Open Hackathons</div>
                 
