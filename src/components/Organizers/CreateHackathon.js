6c43080b407becf4939526c4246b7eaf7b83bcbf
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-01-28 13:15:47 -0500

    implements full validation on createHackathon form. Routes delete hackathon back to user profile

--- src/components/Organizers/CreateHackathon.js
+++ src/components/Organizers/CreateHackathon.js
@@ -176,31 +176,24 @@ const CreateHackathon = props => {
    const [page1Info, setPage1Info] = useState({});
    const [start_date, setStart_date] = useState(`${new Date()}`);
    const [end_date, setEnd_date] = useState(`${new Date()}`);
-   const [hackathonInfo, setHackathonInfo] = useState({
-      name: '',
-      description: '',
-      location: '',
-      url: '',
-      start_date: '',
-      end_date: '',
-      is_open: ''
-   });
+   const [hackathonInfo, setHackathonInfo] = useState({});
    const [state, setState] = useState({ is_open: true });
    const [max, setMax] = useState('');
    const { loading, user } = useAuth0();
    const dispatch = useDispatch();
    const classes = useStyles();
    const [activeStep, setActiveStep] = React.useState(0);
+   const [nameLength, setNameLength] = React.useState(true)
+   const [descLength, setDescLength] = React.useState(true)
+   const [locationLength, setLocationLength] = React.useState(true)
+
 
    
    let { register, handleSubmit, errors, clearError } = useForm();
 
    useEffect(() => {
       setHackathonInfo({
-         name: `${page1Info.name}`,
-         description: `${page1Info.description}`,
-         location: `${page1Info.location}`,
-         url: `${page1Info.url}`,
+         ...page1Info,
          start_date: `${start_date}`,
          end_date: `${end_date}`,
          is_open: state.is_open
@@ -208,6 +201,9 @@ const CreateHackathon = props => {
    }, [page1Info, start_date, end_date, state]);
 
    const handlePage1Change = e => {
+      if (page1Info.hasOwnProperty('name') && page1Info.name.trim().length) setNameLength(true)
+      if (page1Info.hasOwnProperty('description') && page1Info.description.trim().length) setDescLength(true)
+      if (page1Info.hasOwnProperty('location') && page1Info.location.trim().length) setLocationLength(true)
       setPage1Info({ ...page1Info, [e.target.name]: e.target.value });
    };
 
@@ -236,9 +232,18 @@ const CreateHackathon = props => {
       }
    }
 
-
+console.log(page1Info)
    const handleNext = () => {
-      setActiveStep(prevActiveStep => prevActiveStep + 1);
+       if (!page1Info.hasOwnProperty('name') || !page1Info.name.trim().length) {
+         setNameLength(false)
+       } if (!page1Info.hasOwnProperty('description') || !page1Info.description.trim().length) {
+         setDescLength(false)
+       } if (!page1Info.hasOwnProperty('location') || !page1Info.location.trim().length) {
+         setLocationLength(false)
+       } 
+       else setActiveStep(prevActiveStep => prevActiveStep + 1)
+       console.log(nameLength, descLength, locationLength)
+       console.log(page1Info)
    };
 
    const handleBack = () => {
@@ -275,7 +280,7 @@ const CreateHackathon = props => {
                      <TextField
                         type="text"
                         fullWidth
-                        label="Hackathon Name"
+                        label="Hackathon Name (required)"
                         name="name"
                         variant="filled"
                         margin="dense"
@@ -289,7 +294,7 @@ const CreateHackathon = props => {
                            )
                         }}
                      />
-               
+                     {!nameLength ? <p className='create-error'>Please include a name</p> : null}
                      <TextField
                         className={classes.label}
                         type="text"
@@ -298,7 +303,7 @@ const CreateHackathon = props => {
                         rows="4"
                         name="description"
                         variant="filled"
-                        label='Hackathon Description'
+                        label='Hackathon Description (required)'
                         margin="dense"
                         defaultValue={page1Info.description}
                         onChange={handlePage1Change}
@@ -311,7 +316,9 @@ const CreateHackathon = props => {
                            )
                         }}
                      />
+                      
                   </label>
+                  {!descLength ? <p className='create-error'>Please include a description</p> : null}
                   <label className="location-input">
 
                      <TextField
@@ -321,7 +328,7 @@ const CreateHackathon = props => {
                         name="location"
                         variant="filled"
                         margin="dense"
-                        label='Hackathon Location'
+                        label='Hackathon Location (required)'
                         defaultValue={page1Info.location}
                         onChange={handlePage1Change}
                         inputRef={register}
@@ -333,6 +340,7 @@ const CreateHackathon = props => {
                            )
                         }}
                      />
+                       {!locationLength ? <p className='create-error'>Please include a location (ex. San Francisco, Online, etc)</p> : null}
                   </label>
                   <label className="url">
 
